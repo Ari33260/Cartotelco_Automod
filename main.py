978036a1d3e3837a3f753193f1a8018bd430920d
Ari33260 <96233468+Ari33260@users.noreply.github.com>
2025-05-02 21:57:23 +0200

    modif

--- main.py
+++ main.py
@@ -221,7 +221,7 @@ async def on_message(message):
         if message.channel.id == SALON_PARTAGE_ACTU:
             if 'http' in message.content:
                 print("Oui, tu es dans la condition")
-                await Thread.send(content="test")
+                await discord.Thread.send(content="test")
             else:
                 print("Non, tu n'es pas dans la condition")
 
